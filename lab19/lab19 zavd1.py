ef5f52a766f26b7d72ab327c8be79cc88180a015
lesha13 <85362539+lesha13@users.noreply.github.com>
2021-12-19 17:52:01 +0200

    Update lab19 zavd1.py

--- lab19/lab19 zavd1.py
+++ lab19/lab19 zavd1.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 with open('zavd1') as f:
     summ = sum(map(lambda x: float(x), f.read().split('\n')))
     f.seek(0)
     num = len(list(map(lambda x: float(x), f.read().split('\n'))))
     print(f'Середнє арифметичне: {summ/num}')
-=======
-with open('zavd1') as f:
-    summ = sum(map(lambda x: float(x), f.read().split('\n')))
-    f.seek(0)
-    num = len(list(map(lambda x: float(x), f.read().split('\n'))))
-    print(f'Середнє арифметичне: {summ/num}')
->>>>>>> 6f5b3d6b309291675d532587588ce2ca77b26ff3
