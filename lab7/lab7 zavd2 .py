4a3ec8a4a97cb3908d4e919f281a1a957e10a776
lesha13 <leshamashuna@gmail.com>
2021-12-19 17:17:31 +0200

    corrected

--- lab7/lab7 zavd2 .py
+++ lab7/lab7 zavd2 .py
@@ -7,10 +7,14 @@ j = int(input('Стовпці: '))
 x = int(input('x: '))
 m = [[0 for y in range(j)] for z in range(i)]
 
+product = 1
+
 for y in range(i):
     for z in range(j):
-        m[y][z] = y*(sin(y*z) + cos(z*x))
+        m[y][z] = y*(sin(y*x) + cos(z*x))
+        if y*z < x:
+            product *= m[y][z]
 
 m = np.array(m)     # Для красоти
 print(m)
-
+print(product)
