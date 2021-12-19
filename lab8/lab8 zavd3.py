34d52339a60da34bb097c2b6f1151085777b21f1
lesha13 <leshamashuna@gmail.com>
2021-12-19 17:45:52 +0200

    corrected

--- lab8/lab8 zavd3.py
+++ lab8/lab8 zavd3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from random import randint
 
 
@@ -13,19 +12,3 @@ def x(n):
 
 n = randint(0, 10)
 print(f'result = {x(n)}')
-=======
-from random import randint
-
-
-def x(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 9
-    elif n > 1:
-        return 2 * x(n-1) + 3 * x(n-2)
-
-
-n = randint(0, 10)
-print(f'result = {x(n)}')
->>>>>>> 6f5b3d6b309291675d532587588ce2ca77b26ff3
