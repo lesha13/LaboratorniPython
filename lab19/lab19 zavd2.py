7c84c618d2fbf9e7f40e89fde54c65b6b81af5e6
lesha13 <85362539+lesha13@users.noreply.github.com>
2021-12-19 17:52:17 +0200

    Update lab19 zavd2.py

--- lab19/lab19 zavd2.py
+++ lab19/lab19 zavd2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 with open('zavd2') as f:
     print(max(filter(lambda x: x < 0, map(lambda y: float(y), f.read().split('\n')))))
     f.seek(0)
@@ -6,12 +5,4 @@ with open('zavd2') as f:
     with open('V.dat', 'w') as f2:
         for z in numbers:
             f2.write(str(z) + '\n')
-=======
-with open('zavd2') as f:
-    print(max(filter(lambda x: x < 0, map(lambda y: float(y), f.read().split('\n')))))
-    f.seek(0)
-    numbers = filter(lambda x: x < 0, map(lambda y: float(y), f.read().split('\n')))
-    with open('V.dat', 'w') as f2:
-        for z in numbers:
-            f2.write(str(z) + '\n')
->>>>>>> 6f5b3d6b309291675d532587588ce2ca77b26ff3
+          
